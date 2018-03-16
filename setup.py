90e7ffc401d8f959f59e218142b3cdc9bf1ce91b
yuelong.chen <yuelong.chen@yahoo.com>
2018-03-16 11:36:28 +0800

    生成setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ppmonitor',
+    version='0.0.1_beta',
+    packages=[''],
+    url='',
+    license='',
+    author='yuelong.chen',
+    author_email='yuelong_chen@yahoo.com',
+    description=''
+)
